0fd1c485d996491cad385204ef7cee0bc1ab9aa0
Joshua Jensch <doktorjayjay@gmail.com>
2016-02-18 23:46:10 +0100

    Added struct for cpu registers and defined NULL

--- include/types.h
+++ include/types.h
@@ -16,3 +16,14 @@ typedef char* string;
 
 #define low_16(address) (uint16)((address) & 0xFFFF)
 #define high_16(address) (uint16)(((address) >> 16) & 0xFFFF)
+
+
+#define NULL 0UL
+
+/* Registers */
+struct regs {
+        unsigned int gs, fs, es, ds;
+        unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+        unsigned int int_no, err_code;
+        unsigned int eip, cs, eflags, useresp, ss;
+};
